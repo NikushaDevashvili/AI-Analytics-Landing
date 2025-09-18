47bb0f138ebe00bcc1160258493ea38f808dd9f1
Nick Devashvili <nikadevashvili@gmail.com>
2025-09-19 00:10:28 +0400

    adding new section, sdk installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
 import Faqs from "@/sections/Faqs";
 import Functional from "@/sections/Functional";
+import Integration from "@/sections/Integration";
+import LogoTicker from "@/sections/LogoTicker";
 
 export default function Home() {
     return (
@@ -11,6 +13,7 @@ export default function Home() {
             <Navbar />
             <Hero />
             <Functional />
+            <Integration />
             {/* <LogoTicker /> */}
             <Features />
             <Introduction />
